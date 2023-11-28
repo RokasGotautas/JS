6c39802d518a5d92fc67e0fca03c588b3cd2d30b
RokasGotautas <149367955+RokasGotautas@users.noreply.github.com>
2023-11-28 23:48:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1.
+
+let firstName = "Jonas";
+let lastName = "Jonaitis"
+
+console.log(firstName);
+console.log(lastName);
+
+// 2.
+
+let myName = "Rokas";
+let myLastName = "Gotautas";
+let myBirthday = "2000";
+let thisYear = "2023";
+let howOld = 2023-2000;
+
+console.log(`Aš esu ${myName} ${myLastName}. Man yra ${howOld} metai.`)
+
+// 3.
+
+let theName = "Vardenis"
+let nameLast = "Pavardenis";
+let combinedName = theName[0] + nameLast[0];
+
+console.log(theName[0])
+console.log(nameLast[0])
+console.log(combinedName)
+
+// 4.
+
+let actorName = "John"
+let actorLastName = "Cena"
+let lastThreeName = actorName.slice(-3);
+let lastThreeSurname = actorLastName.slice(-3);
+let combinedLastThree = lastThreeName + lastThreeSurname;
+
+console.log(combinedLastThree)
+
+// 5.
